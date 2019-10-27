7974b97c86495c64d58ab0ef30f50cb5858870a4
Charlie Jiang <cqjjjzr@126.com>
2019-10-27 12:47:08 +0800

    ⚡️ Perf: Don't setup test data when generating CMake

--- tests/tests.cmake
+++ tests/tests.cmake
@@ -65,13 +65,16 @@ else()
 endif()
 
 message(STATUS "Aegisub Build: Bootstrapping tests, preparing datas...")
+add_custom_target(test-setup)
 if(WIN32)
-    execute_process(
+    add_custom_command(
+        TARGET test-setup
         COMMAND "${PROJECT_SOURCE_DIR}/tests/setup.bat"
         WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}/tests"
     )
 else()
-    execute_process(
+    add_custom_command(
+        TARGET test-setup
         COMMAND "${PROJECT_SOURCE_DIR}/tests/setup.sh"
         WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}/tests"
     )
